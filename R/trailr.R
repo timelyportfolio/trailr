f2e12087c897368309fb5fee74aa90857abe3335
timelyportfolio <kent.russell@timelyportfolio.com>
2015-02-16 21:29:24 -0600

    skeleton on  a widget

---
+++ R/trailr.R
@@ -0,0 +1,38 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+trailr <- function(message, width = NULL, height = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'trailr',
+    x,
+    width = width,
+    height = height,
+    package = 'trailr'
+  )
+}
+
+#' Widget output function for use in Shiny
+#'
+#' @export
+trailrOutput <- function(outputId, width = '100%', height = '400px'){
+  shinyWidgetOutput(outputId, 'trailr', width, height, package = 'trailr')
+}
+
+#' Widget render function for use in Shiny
+#'
+#' @export
+renderTrailr <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  shinyRenderWidget(expr, trailrOutput, env, quoted = TRUE)
+}
